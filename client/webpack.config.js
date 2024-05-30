b5886e406adcfb2ad505beb3026b7140e159a886
Salokin <nicholas.dpoul@gmail.com>
2024-05-30 17:35:30 -0400

    added manifest.json to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+// Requires the InjectManifest class of the WorkBoxPlugin
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -18,14 +19,36 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates the html file and injects the bundles.
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin'
+        title: 'J.A.T.E'
       }),
-      new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
-      }),
+     // Injects the custom service worker
+     new InjectManifest({
+      swSrc: "./src-sw.js",
+      swDest: "src-sw.js",
+    }),
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'J.A.T.E',
+      description: 'Edit any text!',
+      favicon: "./favicon.ico",
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
     ],
 
     module: {
@@ -40,7 +63,11 @@ module.exports = () => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             }
           }
         },
